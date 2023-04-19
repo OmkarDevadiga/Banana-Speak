b934b171229035cf5c188053732da59413ad81c2
OmkarDevadiga <omkardevadiga@gmail.com>
2023-04-19 18:07:08 +0530

    taking input

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
+
+
+
+var textInput = document.querySelector("#txt-input");
+
+
 btnTranslate.addEventListener("click", function(){
     console.log("clicked");
+    console.log(textInput.value);
 })
