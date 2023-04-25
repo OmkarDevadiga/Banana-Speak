4d823d94b2674b2b33771c66ccdefbee0e592551
OmkarDevadiga <omkardevadiga@gmail.com>
2023-04-25 10:36:03 +0530

    Reset button added

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var btnReset = document.querySelector("#btn-reset");
 var textInput = document.querySelector("#txt-input");
 var textOutput = document.querySelector("#output");
 
+textInput.focus();
+
+function constructUrl(text){
+    var url = "https://api.funtranslations.com/translate/yoda.json?text=" + text;
+    return url;
+}
+
 
 function clickHandler(){
-    textOutput.innerText = "!@$%@%@!$!&@&!@&@! " + textInput.value ;
+    var input = textInput.value;
+    var output = fetch(constructUrl(input))
+                .then(response => response.json())
+                .then(json => {
+                    var translatedText = json.contents.translated;
+                    textOutput.innerText = translatedText;                       
+                    })
 };
 
+
+
 btnTranslate.addEventListener("click", clickHandler);
+
+function reset(){
+    textInput.value = '';
+    textOutput.textContent = '';
+}; 
+
+btnReset.addEventListener("click", reset);
